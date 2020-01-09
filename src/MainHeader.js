d08f9e43540b792c576a0c34e50ed41dfe37f08b
keiya01 <keiya.s.0210@gmail.com>
2020-01-09 14:44:18 +0900

    Update to add loading view to MainHeader component

--- src/MainHeader.js
+++ src/MainHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useState, useDeferredValue } from "react";
 import styled from "styled-components";
 import SearchForm from "./SearchForm";
 
@@ -15,12 +15,45 @@ const Header = styled.header`
 	box-shadow: 0px 2px 5px #212121;
 `;
 
-const MainHeader = () => {
+const LoadingHeader = styled.div`
+	position: fixed;
+	top: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	background-color: #5dab29;
+	padding: 3px 0;
+`;
+
+const LoadingText = styled.p`
+	font-size: 13px;
+	color: #fff;
+`;
+
+export const MainHeaderContext = createContext({ isLoading: false, setIsLoading: () => {} });
+
+const MainHeader = (props) => {
+	const [ isLoading, setIsLoading ] = useState(false);
+	const contextValue = {
+		isLoading,
+		setIsLoading
+	};
+	const deferredIsLoading = useDeferredValue(isLoading, { timeoutMs: 300 });
+
 	return (
-		<Header>
-			<div style={{ display: "flex", flex: 1 }} />
-			<SearchForm />
-		</Header>
+		<MainHeaderContext.Provider value={contextValue}>
+			<Header>
+				<div style={{ display: "flex", flex: 1 }} />
+				<SearchForm />
+			</Header>
+			{deferredIsLoading && (
+				<LoadingHeader>
+					<LoadingText>loading now ...</LoadingText>
+				</LoadingHeader>
+			)}
+			{props.children}
+		</MainHeaderContext.Provider>
 	);
 };
 
