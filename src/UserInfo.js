6b935847428a7898e4cedb8b2afeac38b7943e93
keiya01 <keiya.s.0210@gmail.com>
2020-01-09 13:25:15 +0900

    Fix UserInfo component

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,11 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	display: flex;
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`;
+
+const UserNameWrapper = styled.div`
+	display: inline-block;
+	padding-bottom: 10px;
+`;
+
+const UserName = styled.h1`
+	font-size: 30px;
+	color: #fff;
+`;
 
 const UserInfo = (props) => {
 	const userInfo = props.resouces.detail.read();
 	return (
-		<div>
-			<h1>{userInfo.name}</h1>
-		</div>
+		<Container>
+			<UserNameWrapper>
+				<UserName>{userInfo.name}</UserName>
+			</UserNameWrapper>
+		</Container>
 	);
 };
 
